dd9c030079c57567b57bfe836896f34d07458b4f
jibbs1703 <abrahamjibbs1703@gmail.com>
2025-11-29 10:37:14 -0500

    add simplepackage to test publishing workflow

---
+++ simplepackage/__init__.py
@@ -0,0 +1,7 @@
+"""simplepackage init module."""
+
+from .simple import hello
+
+__all__ = ["__version__", "hello"]
+
+__version__ = "0.1.0"
---
+++ simplepackage/simple.py
@@ -0,0 +1,12 @@
+"""simple module for the simplepackage package."""
+
+def hello(name: str = "world") -> str:
+    """Return a friendly greeting.
+
+    Args:
+        name: Name to include in the greeting.
+
+    Returns:
+        A greeting string.
+    """
+    return f"Hello, {name}!"
